5f5a7f35ba83a86405c7b3c400e6d9d80a53aba2
erika_anrrango <erika.anrrango@epn.edu.ec>
2026-06-17 19:45:08 -0500

    create: configuration environment backend and frontend minimal

---
+++ catalog_customers/src/main/java/com/paco/catalog_customers/CatalogCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.paco.catalog_customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogCustomersApplication.class, args);
+	}
+
+}
